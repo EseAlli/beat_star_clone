7af73793a681093c30663d2e1ce0637d0ca02fb4
EseAlli <essealli.ea@gmail.com>
2022-02-01 15:04:20 +0100

    intial commit and code base setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './styles/app.scss';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer';
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return <div className="footer flex flex-col">
+      <div>
+          <p>Yes! Send me personalized tips for shopping and selling on BeatStars.</p>
+          <input placeholder="Enter your email"/>
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+  </div>;
+};
+
+export default Footer;
